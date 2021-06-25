1983c6fb377ae5abb76857630431fdc4fc24cd6c
LucyMerlier <lucymerlier@gmail.com>
2021-06-25 21:11:12 +0200

    added favicon

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,20 @@ Encore
     // public path used by the web server to access the output path
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
+    // .setManifestKeyPrefix('build/')
+
+    .copyFiles({
+        from: './assets/images',
+
+        // optional target path, relative to the output dir
+        // to: 'images/[path][name].[ext]',
+
+        // if versioning is enabled, add the file hash too
+        to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        // pattern: /\.(png|jpg|jpeg)$/
+    })
 
     /*
      * ENTRY CONFIG
@@ -55,21 +68,20 @@ Encore
         config.corejs = 3;
     })
 
+// uncomment if you use TypeScript
+// .enableTypeScriptLoader()
+
+// uncomment if you use React
+// .enableReactPreset()
+
+// uncomment to get integrity="..." attributes on your script & link tags
+// requires WebpackEncoreBundle 1.4 or higher
+// .enableIntegrityHashes(Encore.isProduction())
+
+// uncomment if you're having problems with a jQuery plugin
+// .autoProvidejQuery()
+
     // enables Sass/SCSS support
-    .enableSassLoader()
-
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
-
-    // uncomment if you use React
-    //.enableReactPreset()
-
-    // uncomment to get integrity="..." attributes on your script & link tags
-    // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
-
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
-;
+    .enableSassLoader();
 
 module.exports = Encore.getWebpackConfig();
