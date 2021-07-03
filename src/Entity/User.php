a7e1fb1f0318f7830ef5b75e07e17b2336994762
LucyMerlier <lucymerlier@gmail.com>
2021-07-03 15:00:55 +0200

    changed getusername

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -120,7 +120,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier(): string
     {
-        return (string) $this->email;
+        return (string)$this->email;
     }
 
     /**
@@ -130,7 +130,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUsername(): ?string
     {
-        return $this->pseudonym ?? $this->email;
+        return (string)$this->email;
     }
 
     /**
