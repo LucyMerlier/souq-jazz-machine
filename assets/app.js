838577aad359a23373b284910c977090cc218b0b
LucyMerlier <lucymerlier@gmail.com>
2021-07-19 15:16:49 +0200

    fix display toast

--- assets/app.js
+++ assets/app.js
@@ -13,14 +13,18 @@ import './bootstrap';
 
 const bootstrap = require('bootstrap');
 
+const toastList = [].slice.call(document.querySelectorAll('.toast'));
+if (!sessionStorage.noToast) {
+    toastList.map((toast) => (new bootstrap.Toast(toast)).show());
+} else {
+    toastList.forEach((toast) => {
+        toast.style.display = 'none';
+    });
+}
+
 const dismissButtons = document.getElementsByClassName('btn-dismiss');
 dismissButtons.forEach((dismiss) => {
     dismiss.addEventListener('click', () => {
         sessionStorage.setItem('noToast', true);
     });
 });
-
-const toastList = [].slice.call(document.querySelectorAll('.toast'));
-if (!sessionStorage.noToast) {
-    toastList.map((toast) => (new bootstrap.Toast(toast)).show());
-}
