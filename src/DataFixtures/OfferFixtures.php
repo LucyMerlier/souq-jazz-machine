3f757167dbc2b781e04afac63214b426d16122d9
LucyMerlier <lucymerlier@gmail.com>
2021-07-20 16:58:18 +0200

    added offers fixtures

---
+++ src/DataFixtures/OfferFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\DataFixtures\InstrumentFixtures;
+use App\Entity\Offer;
+use DateTimeImmutable;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class OfferFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $offer = new Offer();
+        $offer->setInstrument($this->getReference('trombone'));
+        $offer->setDescription(
+            'Tu joues du trombone basse?
+            Tu rêves de faire vibrer les foules en folie?
+            Ne cherche plus! Le Souq\' est fait pour toi!'
+        );
+        $offer->setCreatedAt(new DateTimeImmutable('now'));
+        $manager->persist($offer);
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [InstrumentFixtures::class];
+    }
+}
