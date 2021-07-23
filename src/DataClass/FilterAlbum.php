68be1c7048ad94b26f73a80b370c395e138fffd1
LucyMerlier <lucymerlier@gmail.com>
2021-07-23 15:00:41 +0200

    added album filter

---
+++ src/DataClass/FilterAlbum.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\DataClass;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class FilterAlbum
+{
+    public const CHOICES = [
+        'Les + récents' => 'dateDescending',
+        'Les + anciens' => 'dateAscending',
+        'Alphabétique' => 'titleAscending',
+        'Anti-alphabétique' => 'titleDescending',
+    ];
+
+    /**
+     * @Assert\Choice(choices=self::CHOICES)
+     */
+    private ?string $sort = '';
+
+    /**
+     * @Assert\Length(max=100)
+     */
+    private ?string $query = '';
+
+    public function getSort(): ?string
+    {
+        return $this->sort;
+    }
+
+    public function setSort(?string $sort): self
+    {
+        $this->sort = $sort;
+
+        return $this;
+    }
+
+    public function getQuery(): ?string
+    {
+        return $this->query;
+    }
+
+    public function setQuery(?string $query): self
+    {
+        $this->query = $query;
+
+        return $this;
+    }
+}
