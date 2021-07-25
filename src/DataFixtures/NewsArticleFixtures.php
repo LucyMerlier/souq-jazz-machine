f476032a4aa952c2fdca94258ee1e6baa0a8c373
LucyMerlier <lucymerlier@gmail.com>
2021-07-25 20:26:23 +0200

    fix news fixtures

--- src/DataFixtures/NewsArticleFixtures.php
+++ src/DataFixtures/NewsArticleFixtures.php
@@ -17,8 +17,8 @@ class NewsArticleFixtures extends Fixture
         $newsArticle->setContent('<p>Le <strong>13 juillet 2021</strong> aura lieu le traditionnel BBQ chez Lolo,
         notre bien aimé chef d\'orchestre! Ce sera l\'occasion de se retrouver
         après une année vide d\'événements <i>big-band-esques</i>,
-        et de discuter de l\'année à venir, qui nous l\'espérons sera bien plus fructueuse!</p>
-        <p>Surveillez donc bien les actus dans les mois qui viennent, et à la vôtre!</p>
+        et de discuter de l\'année à venir, qui nous l\'espérons sera bien plus fructueuse!</p><p>
+        Surveillez donc bien les actus dans les mois qui viennent, et à la vôtre!</p>
         ');
         $manager->persist($newsArticle);
 
